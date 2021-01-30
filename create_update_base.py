ec19fd895fcfa0ed5fbe040e53dc20434b201433
Igor <igor.yui@yandex.ru>
2021-01-30 19:12:51 +0300

    Незначительные изменения вывода драйвера БД

--- create_update_base.py
+++ create_update_base.py
@@ -77,18 +77,23 @@ def loadQuery(name: str):
         return query.read()
 
 
+def print_version_DB(action='current'):
+    print(f"{action} version DB {'.'.join(getVersionDB())}")
+
+
 def Execute(file_query: str, action: str):
     conn = Connection()
     cur = conn.cursor()
     cur.execute(loadQuery(file_query))
     conn.commit()
-    print(f'migration: {file_query}\n'
-          f"{action} version DB to ver{'.'.join(getVersionDB())}")
+    print(f'\tmigration: {file_query}')
+    print_version_DB(f'\t{action}')
 
 
 def updateDB():
 
     print('Update DB:')
+    print_version_DB()
     info = getInfo()
 
     last_update = f"up_{info['version'][0]}_{info['version'][1]}.sql"
@@ -96,20 +101,26 @@ def updateDB():
     list_for_update = list(filter(lambda x: x > last_update,
                                   list_up_migrations))
 
-    for file_name in list_for_update:
-        Execute(file_name, 'update')
+    if not list_for_update:
+        print('no updates found')
+    else:
+        for file_name in list_for_update:
+            Execute(file_name, 'update to')
 
     print('')
 
 
 def clearDB():
     print('Deleting DB')
+    print_version_DB()
     info = getInfo()
     list_down_migration = info['down']
     current_downgrade = f"down_{info['version'][1]}_{info['version'][0]}.sql"
     list_for_downgrade = list(filter(lambda x: x <= current_downgrade,
                                      list_down_migration))
+    if not list_for_downgrade:
+        print('database is already on the first change')
     for file_name in list_for_downgrade:
-        Execute(file_name, 'downgrade')
+        Execute(file_name, 'downgrade to')
 
     print('')
