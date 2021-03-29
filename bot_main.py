0f588942b4cb71f2f77e3b14cd89246e20cc5d93
Igor <igor.yui@yandex.ru>
2021-03-29 05:55:12 +0300

    update bot_main to working version

--- bot_main.py
+++ bot_main.py
@@ -19,3 +19,6 @@ def start_handler(message):
 @bot.message_handler(commands=['help'])
 def help_handler(message):
     bot.send_message(message.from_user.id, help_mess)
+
+if __name__ == '__main__':
+    main(True, 'DEBUG')
